6f0d91f92bee5045b09406580db7f0890a6c37b2
gitetsu <enhanced.ttssd@gmail.com>
2011-12-01 02:33:47 +0900

    update init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,17 +1,41 @@
-(setq load-path (cons "~/.emacs.d/elisp" load-path))
+;; load-pathを追加する関数を定義
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory (expand-file-name (concat user-emacs-directory path))))
+        (add-to-list 'load-path default-directory)
+	(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+            (normal-top-level-add-subdirs-to-load-path))))))
 
-(global-set-key "\C-h" 'delete-backward-char)
+;; elispとconfディレクトリをサブディレクトリごとにload-pathに追加
+(add-to-load-path "elisp" "conf")
+
+;; (install-elisp "http://www.emacswiki.org/emacs/download/auto-install.el")
+(when (require 'auto-install nil t)
+  (setq auto-install-directory "~/.emacs.d/elisp/")
+  (auto-install-update-emacswiki-package-name t)
+  (auto-install-compatibility-setup))
+
+(keyboard-translate ?\C-h ?\C-?)
+(global-set-key (kbd "C-x ?") 'help-command)
 
 (setq tab-width 4)
 
+(set-language-environment "Japanese")
 (prefer-coding-system 'utf-8-unix)
+(setq file-name-coding-system 'utf-8)
+(setq locale-coding-system 'utf-8)
+
+(setq show-paren-delay 0)
+(show-paren-mode t)
+(setq show-paren-style 'expression)
+
 
 (global-font-lock-mode t)
 
 (setq-default transient-mark-mode t)
 
-(line-number-mode t)
-(column-number-mode t)
+(global-linum-mode)
 
 ;; install-elisp
 (require 'install-elisp)
@@ -21,18 +45,22 @@
 (require 'auto-complete)
 (global-auto-complete-mode t)
 
-;; linum
-(require 'linum)
-(global-linum-mode)
-
 ;; jaspace
 (require 'jaspace)
 (setq jaspace-alternate-eol-string "<\n")
 (setq jaspace-highlight-tabs ">")
 
 ;; molokai
+;;(when (require 'color-theme nil t)
 (load "~/.emacs.d/elisp/color-theme-molokai.el")
-(color-theme-molokai)
+;;  (color-theme-initialize)
+  (color-theme-molokai)
+
+;; redo+
+;; http://www.emacswiki.org/emacs/download/redo%2b.el
+(when (require 'redo+ nil t)
+  (global-set-key (kbd "C-'") 'redo))
+
 
 ;; php-mode
 (load-library "php-mode")
