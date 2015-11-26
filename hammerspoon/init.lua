e73f335fe5f00d78e7e554ae3e732ae657046520
gitetsu <gitetsu@users.noreply.github.com>
2015-11-26 15:36:23 +0900

    Add application shortcut to hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -15,7 +15,24 @@ function reloadConfig(files)
   end
 end
 
+function focusWithMouse(app)
+  if hs.application.launchOrFocus(app) then
+    if #hs.screen.allScreens() > 1 then
+      local screen = hs.window.focusedWindow():screen()
+      local geo = screen:frame()
+      hs.mouse.setRelativePosition({x=geo.w / 2, y=geo.h / 2}, screen)
+    end
+  end
+end
+
 configFileWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig)
 configFileWatcher:start()
 
 require ("local")
+
+hs.hotkey.bind(modifier(), "a", function() focusWithMouse("Activity Monitor") end)
+hs.hotkey.bind(modifier(), "b", function() focusWithMouse("Firefox") end)
+hs.hotkey.bind(modifier(), "f", function() focusWithMouse("Finder") end)
+hs.hotkey.bind(modifier(), "g", function() focusWithMouse("Google Chrome") end)
+hs.hotkey.bind(modifier(), "i", function() focusWithMouse("iTerm") end)
+hs.hotkey.bind(modifier(), "s", function() focusWithMouse("Skype") end)
