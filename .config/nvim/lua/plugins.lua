e24dc21ebf6c352a8efd8f22350950278c070c50
gitetsu <gitetsu@users.noreply.github.com>
2022-11-25 16:20:12 +0900

    :repeat: nvim: replcace nguyenvukhang/nvim-toggler with monaqa/dial.nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -78,7 +78,6 @@ return require("packer").startup(function(use)
         e = {
           name = "Edit",
           J = { "<cmd>lua require('trevj').format_at_cursor()<cr>", "Split into lines" },
-          t = { "<cmd>lua require('nvim-toggler').toggle()<cr>", "Toggle word" },
         },
         f = {
           name = "Find",
@@ -544,12 +543,42 @@ return require("packer").startup(function(use)
   }
 
   use {
-    "nguyenvukhang/nvim-toggler",
-    module = "nvim-toggler",
+    "monaqa/dial.nvim",
+    event = "BufEnter",
     config = function()
-      require("nvim-toggler").setup {
-        remove_default_keybinds = true,
+      local augend = require "dial.augend"
+      require("dial.config").augends:register_group{
+        default = {
+          augend.integer.alias.decimal,   -- nonnegative decimal number (0, 1, 2, 3, ...)
+          augend.integer.alias.hex,       -- nonnegative hex number  (0x01, 0x1a1f, etc.)
+          augend.constant.alias.bool,
+          augend.date.alias["%Y/%m/%d"],  -- date (2022/02/19, etc.)
+          augend.date.alias["%Y-%m-%d"],
+          augend.date.alias["%m/%d"],
+          augend.date.alias["%H:%M"],
+          augend.date.alias["%H:%M:%S"],
+          augend.constant.alias.ja_weekday,
+          augend.constant.alias.ja_weekday_full,
+
+          augend.constant.new{
+            elements = {"and", "or"},
+            word = true, -- if false, "sand" is incremented into "sor", "doctor" into "doctand", etc.
+            cyclic = true,  -- "or" is incremented into "and".
+          },
+          augend.constant.new{
+            elements = {"&&", "||"},
+            word = false,
+            cyclic = true,
+          },
+        },
       }
+
+      vim.api.nvim_set_keymap("n", "<C-a>", require("dial.map").inc_normal(), { noremap = true })
+      vim.api.nvim_set_keymap("n", "<C-x>", require("dial.map").dec_normal(), { noremap = true })
+      vim.api.nvim_set_keymap("v", "<C-a>", require("dial.map").inc_visual "visual", { noremap = true })
+      vim.api.nvim_set_keymap("v", "<C-x>", require("dial.map").dec_visual "visual", { noremap = true })
+      vim.api.nvim_set_keymap("v", "g<C-a>", require("dial.map").inc_gvisual "visual", { noremap = true })
+      vim.api.nvim_set_keymap("v", "g<C-x>", require("dial.map").dec_gvisual "visual", { noremap = true })
     end,
   }
 
