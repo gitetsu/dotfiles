4f6dcf17fcc784530149312033ca0f4a65a6e23f
gitetsu <gitetsu@users.noreply.github.com>
2022-12-15 12:40:33 +0000

    :art: fix: format

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -236,11 +236,11 @@ return require("packer").startup(function(use)
           Lua = {
             runtime = {
               -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-              version = 'LuaJIT',
+              version = "LuaJIT",
             },
             diagnostics = {
               -- Get the language server to recognize the `vim` global
-              globals = {'vim'},
+              globals = { "vim" },
             },
             workspace = {
               -- Make the server aware of Neovim runtime files
