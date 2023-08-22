b350ab29a19828c540a570fe968f67b2bdac0f55
gitetsu <gitetsu@users.noreply.github.com>
2023-08-22 18:37:37 +0900

    :heavy_plus_sign: :heavy_minus_sign: nvim: p00f/nvim-ts-rainbow :arrow_right: hiphish/rainbow-delimiters.nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -385,19 +385,7 @@ require("lazy").setup({
   },
   {
     -- TODO
-    "p00f/nvim-ts-rainbow",
-    config = function()
-      require("nvim-treesitter.configs").setup {
-        rainbow = {
-          enable = true,
-          disable = { "jsx" },
-          extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-          max_file_lines = nil, -- Do not enable for files with more than n lines, int
-          -- colors = {}, -- table of hex strings
-          -- termcolors = {} -- table of colour name strings
-        },
-      }
-    end,
+    "hiphish/rainbow-delimiters.nvim",
   },
   {
     "m-demare/hlargs.nvim",
