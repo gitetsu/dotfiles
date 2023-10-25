31d02635f4edf017798e3dc1410f504f8ff37941
gitetsu <gitetsu@users.noreply.github.com>
2023-10-26 08:30:40 +0900

    :wrench: nvim: indent-blankline

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -179,11 +179,9 @@ require("lazy").setup({
   },
   {
     "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
     config = function()
-      require("indent_blankline").setup {
-        show_current_context = true,
-        show_current_context_start = true,
-      }
+      require("ibl").setup {}
     end,
   },
   {
