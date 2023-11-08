852fb5e0233d0c021942081d2f3cd99e9e4cbc8a
gitetsu <gitetsu@users.noreply.github.com>
2023-11-09 07:18:52 +0900

    :heavy_plus_sign: / :heavy_minus_sign: nvim: replace hrsh7th/nvim-pasta with gbprod/yanky.nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -482,10 +482,15 @@ require("lazy").setup({
     end,
   },
   {
-    "hrsh7th/nvim-pasta",
+    "gbprod/yanky.nvim",
     config = function()
-      vim.keymap.set({ "n", "x" }, "p", require("pasta.mappings").p)
-      vim.keymap.set({ "n", "x" }, "P", require("pasta.mappings").P)
+      require("yanky").setup {}
+      vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
+      vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
+      vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)")
+      vim.keymap.set({ "n", "x" }, "gP", "<Plug>(YankyGPutBefore)")
+      vim.keymap.set("n", "<c-n>", "<Plug>(YankyCycleForward)")
+      vim.keymap.set("n", "<c-p>", "<Plug>(YankyCycleBackward)")
     end,
   },
   {
